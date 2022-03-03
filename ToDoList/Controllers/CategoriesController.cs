4a01ba5121d66b5888c9161c6721185c2001dbcc
jb <jessb.epicodus@gmail.com>
2022-03-03 10:43:30 -0800

    Objects Within Objects Interface; lesson 36
    S-Espinet.   s.lou.espinet@gmail.com

---
+++ ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    [HttpGet("/categories")]
+    public ActionResult Index()
+    {
+      List<Category> allCategories = Category.GetAll();
+      return View(allCategories);
+    }
+    [HttpPost("/categories")]
+    public ActionResult Create(string categoryName)
+    {
+      Category newCategory = new Category(categoryName);
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/categories/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category selectedCategory = Category.Find(id);
+      List<Item> categoryItems = selectedCategory.Items;
+      model.Add("category", selectedCategory);
+      model.Add("items", categoryItems);
+      return View(model);
+    }
+  }
+}
